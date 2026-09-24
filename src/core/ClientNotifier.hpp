14257515dc6e7da3d193b903806d9a6ace620d28
Mouenis <mouenis2@gmail.com>
2026-09-24 22:49:48 +0100

    feat(core): add ClientNotifier stub

---
+++ src/core/ClientNotifier.hpp
@@ -0,0 +1,12 @@
+#include "../port/ServerOutput/IClientNotifier.hpp"
+
+
+class ClientNotifier: public port::IClientNotifier {
+public:
+    ClientNotifier();
+ 
+    ~ClientNotifier();
+
+private:
+    // members
+};
