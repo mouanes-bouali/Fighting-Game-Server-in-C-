602c32ddc005a5b94b47e140bd2493f8d21e6f66
Mouenis <mouenis2@gmail.com>
2026-09-24 22:49:47 +0100

    test: add Catch2 main entry point

---
+++ test/main.cpp
@@ -0,0 +1,4 @@
+// Catch2 v2 is header-only, but exactly one translation unit must define
+// CATCH_CONFIG_MAIN to provide main(). No library needs to be linked.
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
